1bfcf1fe9469d6d629dd5fb84146cc00a60f0f45
myinan <m.yasir.inan@gmail.com>
2024-03-26 04:15:54 +0300

    Stylize the app

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,15 @@ import PropTypes from "prop-types";
 export default function Header({ currentScore, highestScore }) {
   return (
     <header>
-      <h1>PokeMemo</h1>
+      <h1>
+        <div className="header-text">
+          <span className="poke">Poke</span>
+          <span className="memo">Memo</span>
+        </div>
+      </h1>
       <div className="score-container">
-        <h3>Current Score: {currentScore}</h3>
-        <h3>Highest Score: {highestScore}</h3>
+        <h3>SCORE: {currentScore}</h3>
+        <h3>HIGHEST SCORE: {highestScore}</h3>
       </div>
     </header>
   );
